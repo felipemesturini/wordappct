8e39df07ed6d59a22fb692a803dfd5f9c8eac0d5
fmesturini <felipe_mesturini@outlook.com>
2020-10-19 17:51:57 -0300

    Ajustes na geração do arquivo

--- WCVT.dpr
+++ WCVT.dpr
@@ -10,22 +10,41 @@ uses
   System.IOUtils,
   System.Classes;
 
-function AbrirPlanilha(const AName: String; AHandleCol: TProc<String, String>; out ANewLine: Boolean): Boolean;
+function AbrirPlanilha(const AName: String; AHandleCol: TProc<String, String>): Boolean;
 var
-  lArquivo: TFileStream;
+  lArquivo: TStreamReader;
+  lLinha: string;
+  lLine: Integer;
+  lColunas: TArray<string>;
 begin
-  lArquivo := TFile.OpenRead(AName);
-  ANewLine := False;
-
-
+  lArquivo := TFile.OpenText(AName);
+//  ANewLine := False;
+  lLinha := EmptyStr;
+  lLine := 0;
+  while not lArquivo.EndOfStream do
+  begin
+    lLinha := lArquivo.ReadLine;
+    if (lLine = 0) then begin
+      lColunas := lLinha.Split([';']);
+    end;
+    Inc(lLine);
+  end;
+  lArquivo.Close;
+  lArquivo.Free;
 end;
 
 var
   lWordApp: Variant;
   lNewDoc: Variant;
-
+  lPlanilha: string;
 begin
   try
+    lPlanilha := TPath.Combine(ExtractFilePath(ParamStr(0)), 'Planilha.csv');
+    AbrirPlanilha(lPlanilha,
+      procedure(AColuna, AValor: String)
+      begin
+      end
+    );
     { Creates a Microsoft Word application. }
     lWordApp := CreateOleObject('Word.Application');
     { Creates a new Microsoft Word document. }
